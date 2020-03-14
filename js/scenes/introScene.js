b4c5de587b13960f9da6955bf470f3fc080118fc
c1666033 <whitlowc@cardiff.ac.uk>
2020-03-14 13:35:32 +0000

    updated intro page- web and mobile views

--- js/scenes/introScene.js
+++ js/scenes/introScene.js
@@ -2,18 +2,25 @@ class IntroScene extends Phaser.Scene {
     constructor() {
         super({ key: 'IntroScene' })
     }
-  
     preload() {
         this.load.image('introBackground', 'assets/backgrounds/introBackground.png');
         this.load.image('QR', 'assets/QRcode.png');        
     }
-  
     create() {
         // add title background
         this.introBg = this.add.image(0, 0, 'introBackground').setOrigin(0,0);
         Align.scaleToGameH(this.introBg, 2);
 
-        // page title
+        // add page elements
+        this.titleText();
+        this.introText(this.titleText, 30);
+        this.launchButton(this.introText, 60);
+        this.shareQR(this.launchButton, 60);
+        this.shareURL(this.shareQR, 10);
+        this.githubText(this.shareText, 50)
+        this.githubButton(this.githubText, 10);
+    }
+    titleText(){
         this.titleText = this.make.text({
             x: 0,
             y: 50,
@@ -29,14 +36,14 @@ class IntroScene extends Phaser.Scene {
         });
         Align.scaleToGameW(this.titleText, .8);
         Align.centerH(this.titleText);
-
-        // intro paragraph
-        let introTextY = this.titleText.y + (this.titleText.height * this.titleText._scaleY) + 30;
+    }
+    introText(placeBelow, distance){
+        let introTextY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
         this.introText = this.make.text({
             x: 0,
             y: introTextY,
             padding: { x: 10, y: 10 },
-            text: 'This game has been created as a prototype for a mobile gaming platform to teach core statistical concepts to undergraduate students.\n\nSource code and more information is available at:',
+            text: 'This game is a prototype for a mobile gaming platform to teach core statistical concepts to undergraduate students.',
             style: {
                 fontSize: '22px',
                 fontFamily: 'Arial',
@@ -48,9 +55,74 @@ class IntroScene extends Phaser.Scene {
         });
         Align.scaleToGameW(this.introText, .8);
         Align.centerH(this.introText);
-
-        // github button
-        let githubY = this.introText.y + (this.introText.height * this.introText._scaleY) + 10;
+    }
+    launchButton(placeBelow, distance){
+        let launchY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        this.launchButton = this.make.text({
+            x: 0,
+            y: launchY,
+            padding: { x: 30, y: 30 },
+            text: 'Play the game',
+            style: {
+                fontSize: '64px',
+                fontFamily: 'Arial',
+                color: 'white',
+                align: 'center',
+                backgroundColor: 'black',
+            },
+            add: true
+        });
+        Align.scaleToGameW(this.launchButton, .6);
+        Align.centerH(this.launchButton);
+        this.launchButton.setInteractive().on('pointerup', function () {
+            this.launchGame();
+        }, this);
+    }
+    shareQR(placeBelow, distance){
+        let qrCodeY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        this.shareQR = this.add.image(0, qrCodeY, 'QR').setOrigin(0, 0);
+        Align.scaleToGameW(this.shareQR, 0.5);
+        Align.centerH(this.shareQR);        
+    }
+    shareURL(placeBelow, distance){
+        let shareY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        this.shareText = this.make.text({
+            x: 0,
+            y: shareY,
+            padding: { x: 10, y: 10 },
+            text: 'qrco.de/zombieStats',
+            style: {
+                fontSize: '64px',
+                fontFamily: 'Arial',
+                color: 'black',
+                align: 'center',
+            },
+            add: true
+        });
+        Align.scaleToGameW(this.shareText, .4);
+        Align.centerH(this.shareText);
+    }
+    githubText(placeBelow, distance){
+        let githubTextY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        this.githubText = this.make.text({
+            x: 0,
+            y: githubTextY,
+            padding: { x: 10, y: 10 },
+            text: 'Source code and more information available at:',
+            style: {
+                fontSize: '28px',
+                fontFamily: 'Arial',
+                color: 'black',
+                align: 'center',
+                wordWrap: { width: 350 }
+            },
+            add: true
+        });
+        Align.scaleToGameW(this.githubText, .5);
+        Align.centerH(this.githubText);        
+    }
+    githubButton(placeBelow, distance){
+        let githubY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
         this.github = this.make.text({
             x: 0,
             y: githubY,
@@ -71,57 +143,7 @@ class IntroScene extends Phaser.Scene {
         this.github.setInteractive().on('pointerup', () => {
             window.open('https://github.com/charlywhitlow/stats_vs_zombies', '_blank');
         });
-
-        // launch game
-        let launchY = this.github.y + (this.github.height * this.github._scaleY) + 90;
-        this.launchButton = this.make.text({
-            x: 0,
-            y: launchY,
-            padding: { x: 30, y: 30 },
-            text: 'Play the game',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: 'white',
-                align: 'center',
-                backgroundColor: 'black',
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.launchButton, .6);
-        Align.centerH(this.launchButton);
-        this.launchButton.setInteractive().on('pointerup', function () {
-            this.launchGame();
-        }, this);
-
-        // QR code
-        let qrCodeY = this.launchButton.y + (this.launchButton.height * this.launchButton._scaleY) + 30;
-        this.QR = this.add.image(0, qrCodeY, 'QR').setOrigin(0, 0);
-        Align.scaleToGameW(this.QR, 0.5);
-        Align.centerH(this.QR);
-
-        // share URL
-        let shareY = this.QR.y + (this.QR.height * this.QR._scaleY) + 10;
-        this.shareText = this.make.text({
-            x: 0,
-            y: shareY,
-            padding: { x: 10, y: 10 },
-            text: 'qrco.de/zombieStats',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: 'black',
-                align: 'center',
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.shareText, .4);
-        Align.centerH(this.shareText);
-        // this.shareText.setInteractive().on('pointerup', function () {
-        //     // copy url to clipboard?
-        // }, this);
     }
-
     launchGame(){
         // enter fullscreen
         if (!this.scale.isFullscreen){
