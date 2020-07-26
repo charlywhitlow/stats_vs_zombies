50f509a99d59a7f674a3096c81b840898249c2c6
c1666033 <whitlowc@cardiff.ac.uk>
2020-07-26 12:02:01 +0100

    updated readme and intro pages

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 This project was created as part of a final year project to create a mobile game to teach statistics to undergraduates, using the excellent HTML5 game framework Phaser3: https://phaser.io/phaser3
 
-Please note that many parts of the game have not yet been implemented and this really is only the start of a game.
+The game works best in Chrome, and you will need to view in a mobile browser, or using a mobile emulator, e.g. https://developers.google.com/web/tools/chrome-devtools/device-mode
 
-* The **master** branch contains the beginnings of the HTML5 game, and can be see in action at: http://users.cs.cf.ac.uk/WhitlowC/stats_vs_zombies
+Please note that many parts of the game have not yet been implemented and this is a work in progress.
+
+* The **master** branch contains the the HTML5 game, and can be see in action at: http://users.cs.cf.ac.uk/WhitlowC/stats_vs_zombies
 
 * The **login** branch contains the beginning of a login system for the game in Node.js, and can be viewed at: https://stats-vs-zombies.herokuapp.com
 
--- js/scenes/introScene.js
+++ js/scenes/introScene.js
@@ -12,160 +12,177 @@ class IntroScene extends Phaser.Scene {
         Align.scaleToGameH(this.introBg, 2);
 
         // add page elements
-        this.createTitleText();
-        this.createIntroText(this.titleText, 30);
-        this.createLaunchButton(this.introText, 80);
-        this.createShareText(this.launchButton, 100);
-        this.createShareQR(this.shareText, 20);
-        this.createShareURL(this.shareQR, 20);
-        // this.createGithubText(this.shareURL, 50)
-        // this.createGithubButton(this.githubText, 0);
-    }
-    createTitleText(){
-        this.titleText = this.make.text({
-            x: 0,
-            y: 50,
-            padding: { x: 32, y: 16 },
-            text: 'Stats Vs Zombies',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: 'black',
-                align: 'center',
-            },
-            add: true
+        this.titleText = this.createText(0, 50, "Stats Vs Zombies", .8, {
+            paddingX : 32,
+            paddingY : 16,
+            fontSize : '64px',
+            fontFamily : 'Arial',
+            color : 'black',
+            align : 'center'
         });
-        Align.scaleToGameW(this.titleText, .8);
-        Align.centerH(this.titleText);
-    }
-    createIntroText(placeBelow, distance){
-        let introTextY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.introText = this.make.text({
-            x: 0,
-            y: introTextY,
-            padding: { x: 10, y: 10 },
-            text: 'This game is a prototype for a mobile gaming platform to teach core statistical concepts to undergraduate students.',
-            style: {
-                fontSize: '22px',
-                fontFamily: 'Arial',
-                color: 'black',
-                align: 'center',
-                wordWrap: { width: 340 }
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.introText, .8);
-        Align.centerH(this.introText);
-    }
-    createLaunchButton(placeBelow, distance){
-        let launchY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.launchButton = this.make.text({
-            x: 0,
-            y: launchY,
-            padding: { x: 30, y: 30 },
-            text: 'Play the game',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: 'white',
-                align: 'center',
-                backgroundColor: '#d60007',
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.launchButton, .6);
-        Align.centerH(this.launchButton);
+
+        this.introText = this.createTextBelow(this.titleText, 10, .85,
+            'This game was created as part of a final year university project to teach statistical concepts in a fun and accessible way.', {
+                paddingX : 10,
+                paddingY : 10,
+                fontSize : '20px',
+                fontFamily : 'Arial',
+                color : 'black',
+                align : 'center',
+                wordWrap : 340
+            }
+        );
+
+        this.chromeText = this.createTextBelow(this.introText, 0, .85,
+            'This project is a work in progress and currently works best in Chrome.', {
+                paddingX : 10,
+                paddingY : 10,
+                fontSize : '20px',
+                fontFamily : 'Arial',
+                color : 'black',
+                align : 'center',
+                wordWrap : 340
+            }
+        );
+    
+        this.launchButton = this.createButton(this.chromeText, 20, 0.6, 'Play the game',
+            {
+                paddingX : 25,
+                paddingY : 25,
+                fontSize : '64px',
+                fontFamily : 'Arial',
+                color : 'white',
+                align : 'center',
+                backgroundColor : "#d60007"
+            }
+        );
         this.launchButton.setInteractive().on('pointerup', function () {
             this.launchGame();
         }, this);
-    }
-    createShareText(placeBelow, distance){
-        let shareY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.shareText = this.make.text({
-            x: 0,
-            y: shareY,
-            padding: { x: 10, y: 10 },
-            text: 'Share with your friends:',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: 'black',
-                align: 'center',
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.shareText, .6);
-        Align.centerH(this.shareText);
-    }
-    createShareQR(placeBelow, distance){
-        let qrCodeY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.shareQR = this.add.image(0, qrCodeY, 'QR').setOrigin(0, 0);
-        Align.scaleToGameW(this.shareQR, 0.5);
-        Align.centerH(this.shareQR);
-    }
-    createShareURL(placeBelow, distance){
-        let shareY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.shareURL = this.make.text({
-            x: 0,
-            y: shareY,
-            padding: { x: 10, y: 10 },
-            text: 'users.cs.cf.ac.uk/WhitlowC/ stats_vs_zombies',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: 'blue',
-                align: 'center',
+
+        this.shareText = this.createTextBelow(this.launchButton, 20, .6,
+            'Share with your friends:', {
+                paddingX : 10,
+                paddingY : 10,
+                fontSize : '20px',
+                fontFamily : 'Arial',
+                color : 'black',
+                align : 'center',
+                wordWrap : 340
+            }
+        );
+
+        this.qrImage = this.createImage(this.shareText, 0, 'QR', 0.4);
+
+        this.shareURL = this.createTextBelow(this.qrImage, 10, .8,
+            'users.cs.cf.ac.uk/WhitlowC/ stats_vs_zombies', {
+                paddingX : 10,
+                paddingY : 10,
+                fontSize : '20px',
+                fontFamily : 'Arial',
+                color : 'red',
+                align : 'center',
                 fontStyle: 'bold',
-                wordWrap: { width: 1 }
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.shareURL, .85);
-        Align.centerH(this.shareURL);
+                wordWrap : 1
+            }
+        );
         this.shareURL.setInteractive().on('pointerup', () => {
             window.open('https://users.cs.cf.ac.uk/WhitlowC/stats_vs_zombies/', '_blank');
         });
-    }
-    createGithubText(placeBelow, distance){
-        let githubTextY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.githubText = this.make.text({
-            x: 0,
-            y: githubTextY,
-            padding: { x: 10, y: 10 },
-            text: 'Source code and more information at:',
-            style: {
-                fontSize: '28px',
-                fontFamily: 'Arial',
-                color: 'black',
-                align: 'center',
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.githubText, .8);
-        Align.centerH(this.githubText);
-    }
-    createGithubButton(placeBelow, distance){
-        let githubY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
-        this.github = this.make.text({
-            x: 0,
-            y: githubY,
-            padding: { x: 10, y: 8 },
-            text: "github.com/charlywhitlow/stats_vs_zombies",
-            style: {
-                fontSize: '24px',
-                fontFamily: 'Arial',
-                color: 'blue',
-                align: 'center',
-                borderRound: '15px',
-                fontStyle: 'bold'
-            },
-            add: true
-        });
-        Align.scaleToGameW(this.github, .8);
-        Align.centerH(this.github);
-        this.github.setInteractive().on('pointerup', () => {
+
+        this.githubText = this.createTextBelow(this.shareURL, 10, .5,
+            'Source code available at:', {
+                paddingX : 10,
+                paddingY : 10,
+                fontSize : '20px',
+                fontFamily : 'Arial',
+                color : 'black',
+                align : 'center',
+                wordWrap : 340
+            }
+        );
+
+        this.githubLink = this.createTextBelow(this.githubText, 0, .85,
+            'github.com/charlywhitlow/stats_vs_zombies', {
+                paddingX : 10,
+                paddingY : 0,
+                fontSize : '20px',
+                fontFamily : 'Arial',
+                fontStyle: 'bold',
+                color : 'red',
+                align : 'center',
+                wordWrap : 340
+            }
+        );
+        this.githubLink.setInteractive().on('pointerup', () => {
             window.open('https://github.com/charlywhitlow/stats_vs_zombies', '_blank');
         });
+    }
+    createText(x, y, text, scale, style){
+        let textObj = this.make.text({
+            x: x,
+            y: y,
+            padding: { x: style.paddingX, y: style.paddingY },
+            text: text,
+            style: {
+                fontSize: style.fontSize,
+                fontFamily: style.fontFamily,
+                fontStyle : (style.fontStyle) ? style.fontStyle : 'normal',
+                color: style.color,
+                align: style.align,
+            },
+            add: true
+        });
+        Align.scaleToGameW(textObj, scale);
+        Align.centerH(textObj);
+        return textObj;
+    }
+    createTextBelow(placeBelow, distance, scale, text, style){
+        let textY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        let textObj = this.make.text({
+            x: 0,
+            y: textY,
+            padding: { x: style.paddingX, y: style.paddingY },
+            text: text,
+            style: {
+                fontSize: style.fontSize,
+                fontFamily: style.fontFamily,
+                color: style.color,
+                align: style.align,
+                wordWrap: { width: style.wordWrap }
+            },
+            add: true
+        });
+        Align.scaleToGameW(textObj, scale);
+        Align.centerH(textObj);
+        return textObj;
+    }
+    createButton(placeBelow, distance, scale, text, style){
+        let launchY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        let button = this.make.text({
+            x: 0,
+            y: launchY,
+            padding: { x: style.paddingX, y: style.paddingY },
+            text: text,
+            style: {
+                fontSize: style.fontSize,
+                fontFamily: style.fontFamily,
+                color: style.color,
+                align: style.align,
+                backgroundColor: style.backgroundColor,
+            },
+            add: true
+        });
+        Align.scaleToGameW(button, scale);
+        Align.centerH(button);
+        return button;
+    }
+    createImage(placeBelow, distance, image, scale){
+        let imageY = placeBelow.y + (placeBelow.height * placeBelow._scaleY) + distance;
+        let imageObj = this.add.image(0, imageY, image).setOrigin(0, 0);
+        Align.scaleToGameW(imageObj, scale);
+        Align.centerH(imageObj);
+        return imageObj;
     }
     launchGame(){
         // // enter fullscreen
@@ -174,5 +191,5 @@ class IntroScene extends Phaser.Scene {
         // }
         // launch title scene
         this.scene.start("TitleScene");
-    }
+    }   
 }
